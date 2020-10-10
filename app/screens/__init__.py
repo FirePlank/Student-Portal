3f929de12ad36b942d4d3b0861884966b7f9fbbd
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-10 17:41:26 +0530

    Update __init__.py

--- app/screens/__init__.py
+++ app/screens/__init__.py
@@ -1,9 +1,7 @@
-from app.tools import load_kv
-
+from app.tools import load_kv, resource_path
+from kivy.lang import Builder
 import os
-from kivy.resources import resource_add_path
-resource_add_path(os.path.dirname(os.path.realpath(__file__)))
 
 from .mainmenu import MainMenu
 
-load_kv(__file__, os.path.join('kv', 'mainmenu.kv'))
+Builder.load_file(resource_path(os.path.join('screens', 'kv', 'mainmenu.kv')))
