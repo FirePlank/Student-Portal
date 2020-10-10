b2f6dd4af0197eb04a54cb7cb45e6108764a6d9a
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-10 17:43:11 +0530

    Update __init__.py

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,18 +1,9 @@
-from os.path import dirname, join
+from os.path import dirname, join, abspath
 from kivy.lang import Builder
 import sys
 
-def load_kv(filepath, file):
-    ''' 
-    load a kivy file from the current
-    directory of the file calling this func
-    where filepath is __file__ and file is a kv file
-    ''' 
-    filepath = dirname(filepath)
-    Builder.load_file(join(filepath, file))
-
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """ Get absolute path to resource, works for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
