42358ee48ca1cec8403a10d37626e8e2436f50ca
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-10 14:30:18 +0530

    Add files via upload (#1)
    Main project structure

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from kivy.config import Config
+Config.set('graphics','width',1280)
+Config.set('graphics','height',720)
+
+import os
+import sys
+import json
+
+from kivy.resources import resource_add_path
+resource_add_path(os.path.dirname(os.path.realpath(__file__)))
+
+from kivymd.app import MDApp
+
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+
+from app.screens import MainMenu
+
+class StudentPortal(MDApp):
+    title = "Student Portal"
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Green"
+        self.root = ScreenManager()
+        self.mainmenu = MainMenu()
+        self.root.switch_to(self.mainmenu)
+        return self.root
+
+
+if __name__ == '__main__':
+	StudentPortal().run()
