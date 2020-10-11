e6755e32ea132a2228be328ff08e13c058866eb2
FirePlank <sissala11@gmail.com>
2020-10-11 14:10:13 +0300

    Added Wikipedia backend and requirements.txt

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import sys
 import json
 
 from kivy.resources import resource_add_path
-from app.tools import resource_path
+from tools import resource_path
 resource_add_path(resource_path(os.path.join('data', 'logo')))
 
 from kivymd.app import MDApp
@@ -15,7 +15,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
-from app.screens import MainMenu, Wikipedia
+from screens import MainMenu, Wikipedia
 import random
 
 class StudentPortal(MDApp):
@@ -40,6 +40,45 @@ class StudentPortal(MDApp):
 
     def search_wikipedia(self, query):
         return(str(random.randint(0,10))) # return result, should be string
+
+    class Wikipedia:
+        def __init__(self, keyword):
+            self.S = requests.Session()
+
+            self.URL = "https://en.wikipedia.org/w/api.php"
+
+            self.PARAMS = {
+                "action": "query",
+                "generator": "search",
+                "gsrsearch": keyword,
+                "prop": "extracts|pageimages",
+                "exintro": 1,
+                "format": "json"
+            }
+
+            self.R = self.S.get(url=self.URL, params=self.PARAMS)
+            self.DATA = self.R.json()
+
+        def titles(self):
+            titles = []
+            for i in self.DATA["query"]["pages"]:
+                titles.append(self.DATA["query"]["pages"][i]["title"])
+            return titles
+
+        def first_paragraphs(self):
+            paragraphs = []
+            for i in self.DATA["query"]["pages"]:
+                paragraphs.append(self.DATA["query"]["pages"][i]["extract"])
+            return paragraphs
+
+        def photos(self):
+            pictures = []
+            for i in self.DATA["query"]["pages"]:
+                try:
+                    pictures.append(self.DATA["query"]["pages"][i]["thumbnail"]["source"])
+                except:
+                    pictures.append(None)
+            return pictures
 
 
 if __name__ == '__main__':
