d54b54c8194aea1a5ba2d490a330240818fdfd3c
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-11 16:20:29 +0530

    Update __init__.py

--- app/screens/__init__.py
+++ app/screens/__init__.py
@@ -5,3 +5,4 @@ import os
 from .mainmenu import MainMenu
 
 Builder.load_file(resource_path(os.path.join('screens', 'kv', 'mainmenu.kv')))
+Builder.load_file(resource_path(os.path.join('screens', 'kv', 'wikipedia.kv')))
