7939dbcabfe81ece3f4c769824050ff8211c92e6
FirePlank <44502537+FirePlank@users.noreply.github.com>
2020-10-11 19:26:22 +0300

    Made the wikipedia thing better hopefully xD

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
-from screens import MainMenu, Wikipedia
+from screens import MainMenu, Wikipedia_Main
 import random
 
 class StudentPortal(MDApp):
@@ -26,7 +26,7 @@ class StudentPortal(MDApp):
         self.theme_cls.primary_palette = "Green"
         self.root = ScreenManager()
         self.mainmenu = MainMenu()
-        self.wikipedia = Wikipedia()
+        self.wikipedia = Wikipedia_Main()
         self.screens = {
             'mainmenu': self.mainmenu,
             'wikipedia': self.wikipedia,
@@ -37,49 +37,6 @@ class StudentPortal(MDApp):
     def switch_screen(self, screen_name, direction='left'):
         self.root.transition.direction = direction
         self.root.switch_to(self.screens.get(screen_name))
-
-    def search_wikipedia(self, query):
-        return(str(random.randint(0,10))) # return result, should be string
-
-    class Wikipedia:
-        def __init__(self, keyword):
-            self.S = requests.Session()
-
-            self.URL = "https://en.wikipedia.org/w/api.php"
-
-            self.PARAMS = {
-                "action": "query",
-                "generator": "search",
-                "gsrsearch": keyword,
-                "prop": "extracts|pageimages",
-                "exintro": 1,
-                "format": "json"
-            }
-
-            self.R = self.S.get(url=self.URL, params=self.PARAMS)
-            self.DATA = self.R.json()
-
-        def titles(self):
-            titles = []
-            for i in self.DATA["query"]["pages"]:
-                titles.append(self.DATA["query"]["pages"][i]["title"])
-            return titles
-
-        def first_paragraphs(self):
-            paragraphs = []
-            for i in self.DATA["query"]["pages"]:
-                paragraphs.append(self.DATA["query"]["pages"][i]["extract"])
-            return paragraphs
-
-        def photos(self):
-            pictures = []
-            for i in self.DATA["query"]["pages"]:
-                try:
-                    pictures.append(self.DATA["query"]["pages"][i]["thumbnail"]["source"])
-                except:
-                    pictures.append(None)
-            return pictures
-
 
 if __name__ == '__main__':
 	StudentPortal().run()
--- app/screens/wikipedia.py
+++ app/screens/wikipedia.py
@@ -1,10 +1,53 @@
 from kivymd.uix.screen import MDScreen
 from kivymd.toast import toast
 from kivymd.app import MDApp
+import requests
 
-class Wikipedia(MDScreen):
+
+class Wikipedia:
+    def __init__(self, keyword):
+        self.S = requests.Session()
+
+        self.URL = "https://en.wikipedia.org/w/api.php"
+
+        self.PARAMS = {
+            "action": "query",
+            "generator": "search",
+            "gsrsearch": keyword,
+            "prop": "extracts",
+            "exintro": 1,
+            "format": "json"
+        }
+
+        self.R = self.S.get(url=self.URL, params=self.PARAMS)
+        self.DATA = self.R.json()
+
+    def titles(self):
+        titles = []
+        for i in self.DATA["query"]["pages"]:
+            titles.append(self.DATA["query"]["pages"][i]["title"])
+        return titles
+
+    def first_paragraphs(self):
+        paragraphs = []
+        for i in self.DATA["query"]["pages"]:
+            paragraphs.append(self.DATA["query"]["pages"][i]["extract"])
+        return paragraphs
+
+    def urls(self):
+        urls = []
+        for i in self.DATA["query"]["pages"]:
+            urls.append(f'https://en.wikipedia.org/wiki/{self.DATA["query"]["pages"][i]["title"].replace(" ", "_")}')
+        return urls
+
+    def all(self):
+        all=[]
+        for i in self.DATA["query"]["pages"]:
+            all.append([self.DATA["query"]["pages"][i]["title"], f'https://en.wikipedia.org/wiki/{self.DATA["query"]["pages"][i]["title"].replace(" ", "_")}', self.DATA["query"]["pages"][i]["extract"]])
+
+class Wikipedia_Main(MDScreen):
     def search(self, query):
         if query.strip() == "":
-            toast('What to search? xD', duration=1)
+            toast('What do you want to search?', duration=1)
         else:
-            self.ids.results.text = MDApp.get_running_app().search_wikipedia(query.strip())
+            self.ids.results.text = Wikipedia(query.strip()).all()
