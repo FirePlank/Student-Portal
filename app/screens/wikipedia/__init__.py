5c47578a84d1ad45d6d6b6f979c67041354cb07e
Al Muhimen <44261822+Muhimen123@users.noreply.github.com>
2020-10-12 13:12:32 +0600

    Fixed the wikipedia page (#8)
    * Module not found error fixed
    
    Adding the prefix `app` before few of the imports caused import error.
    However, removing those fixes the error for me. That's why removed them.
    
    * Added database & operator
    
    * Removed kv folder as it was serving no purpose
    
    * Wikipedia returns the summary
    
    * Added the backend class
    
    * Reduced font size of the output field
    
    * minor improvement

--- app/screens/wikipedia/__init__.py
+++ app/screens/wikipedia/__init__.py
@@ -1,6 +1,9 @@
 from kivymd.uix.screen import MDScreen
 from kivymd.toast import toast
 from kivymd.app import MDApp
+import requests
+
+
 
 class Wikipedia(MDScreen):
     def search(self, query):
@@ -8,3 +11,59 @@ class Wikipedia(MDScreen):
             toast('What to search? xD', duration=1)
         else:
             self.ids.results.text = MDApp.get_running_app().search_wikipedia(query.strip())
+
+
+class WikipediaBackend():
+    def __init__(self, keyword):
+        self.S = requests.Session()
+
+        self.URL = "https://en.wikipedia.org/w/api.php"
+
+        self.PARAMS = {
+            "action": "query",
+            "generator": "search",
+            "gsrsearch": keyword,
+            "prop": "extracts",
+            "exintro": 1,
+            "format": "json",
+            "explaintext": True
+        }
+
+        self.R = self.S.get(url=self.URL, params=self.PARAMS)
+        self.DATA = self.R.json()
+
+    def summary(self):
+        try:
+            data = self.DATA["query"]["pages"]
+            for page in data:
+                if data[page]["index"] == 1:
+                    title = data[page]["title"]
+                    summary = data[page]["extract"]
+                    break
+            return title + '\n\n' + summary[:2000] + ('...' if len(summary) > 2000 else '')
+
+        except:
+            return "Sorry, couldn't fetch any search result for that."
+
+    def titles(self):
+        titles = []
+        for i in self.DATA["query"]["pages"]:
+            titles.append(self.DATA["query"]["pages"][i]["title"])
+        return titles
+
+    def first_paragraphs(self):
+        paragraphs = []
+        for i in self.DATA["query"]["pages"]:
+            paragraphs.append(self.DATA["query"]["pages"][i]["extract"])
+        return paragraphs
+
+    def urls(self):
+        urls = []
+        for i in self.DATA["query"]["pages"]:
+            urls.append(f'https://en.wikipedia.org/wiki/{self.DATA["query"]["pages"][i]["title"].replace(" ", "_")}')
+        return urls
+
+    def all(self):
+        all=[]
+        for i in self.DATA["query"]["pages"]:
+            all.append([self.DATA["query"]["pages"][i]["title"], f'https://en.wikipedia.org/wiki/{self.DATA["query"]["pages"][i]["title"].replace(" ", "_")}', self.DATA["query"]["pages"][i]["extract"]])
