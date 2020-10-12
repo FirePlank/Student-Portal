b883d0bd37c342a2043302e2a5df095405d210e0
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-12 09:02:36 +0530

    Project restructure (#6)

--- app/main.py
+++ app/main.py
@@ -7,16 +7,15 @@ import sys
 import json
 
 from kivy.resources import resource_add_path
-from tools import resource_path
+from app.tools import resource_path
 resource_add_path(resource_path(os.path.join('data', 'logo')))
-resource_add_path(resource_path(os.path.join('data', 'fonts')))
 
 from kivymd.app import MDApp
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
-from screens import MainMenu, Wikipedia_Main
+from app.screens import MainMenu, Wikipedia
 import random
 
 class StudentPortal(MDApp):
@@ -27,7 +26,7 @@ class StudentPortal(MDApp):
         self.theme_cls.primary_palette = "Green"
         self.root = ScreenManager()
         self.mainmenu = MainMenu()
-        self.wikipedia = Wikipedia_Main()
+        self.wikipedia = Wikipedia()
         self.screens = {
             'mainmenu': self.mainmenu,
             'wikipedia': self.wikipedia,
@@ -38,6 +37,10 @@ class StudentPortal(MDApp):
     def switch_screen(self, screen_name, direction='left'):
         self.root.transition.direction = direction
         self.root.switch_to(self.screens.get(screen_name))
+
+    def search_wikipedia(self, query):
+        return(str(random.randint(0,10))) # return result, should be string
+
 
 if __name__ == '__main__':
 	StudentPortal().run()
