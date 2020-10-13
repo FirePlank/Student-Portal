c7dbea97e2f14b77a250b8d0af69296ff62e9546
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-13 05:59:28 +0530

    Optimization (#9)

--- app/screens/mainmenu/__init__.py
+++ app/screens/mainmenu/__init__.py
@@ -4,21 +4,17 @@ from kivymd.theming import ThemableBehavior
 from kivymd.uix.button import MDIconButton
 from kivy.properties import NumericProperty
 
+
 class MainMenu(MDScreen):
     pass
 
+
 class CustomIconButton(MDIconButton, ThemableBehavior, HoverBehavior):
-    '''Custom item implementing hover behavior.'''
-    canvas_opacity = NumericProperty(0, rebind=True)
+
+    canvas_opacity = NumericProperty(0)
 
     def on_enter(self, *args):
-        '''The method will be called when the mouse cursor
-        is within the borders of the current widget.'''
-
         self.canvas_opacity = 1
 
     def on_leave(self, *args):
-        '''The method will be called when the mouse cursor goes beyond
-        the borders of the current widget.'''
-
         self.canvas_opacity = 0
