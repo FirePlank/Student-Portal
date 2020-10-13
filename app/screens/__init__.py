82bba6021f711b4050e55d8be61c22abdc4d4be7
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-13 16:00:37 +0530

    Add Notebook GUI (#12)
    added notebook GUI and couple of other changes.
    
    Not functional yet.

--- app/screens/__init__.py
+++ app/screens/__init__.py
@@ -6,11 +6,14 @@ from kivymd.app import MDApp
 if getattr(sys, 'frozen', False):
 	from app.screens.mainmenu import MainMenu
 	from app.screens.wikipedia import Wikipedia
+	from app.screens.notebook import Notebook
 else:
 	from screens.mainmenu import MainMenu
 	from screens.wikipedia import Wikipedia
+	from screens.notebook import Notebook
 
 resource_path = MDApp.get_running_app().resource_path
 
 Builder.load_file(resource_path(os.path.join('screens', 'mainmenu', 'mainmenu.kv')))
 Builder.load_file(resource_path(os.path.join('screens', 'wikipedia', 'wikipedia.kv')))
+Builder.load_file(resource_path(os.path.join('screens', 'notebook', 'notebook.kv')))
