bda8045af5947f3ca5cdf2d5b678313f4c1f5e53
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-14 16:18:02 +0530

    Notebook page done (#14)
    * Notebook page done
    
    YES! DONE! FULLY!
    
    * Made text more proffesional
    
    Co-authored-by: FirePlank <fireplankbusiness@gmail.com>

--- app/screens/notebook/__init__.py
+++ app/screens/notebook/__init__.py
@@ -2,13 +2,73 @@ from ..modules import sql_operator
 from datetime import datetime
 from ..widgets.hover_icon_button import HoverIconButton
 from kivymd.uix.screen import MDScreen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import BooleanProperty
+from kivymd.toast import toast
+from kivy.uix.popup import Popup
 
 
 class Notebook(MDScreen):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.database = NotebookBackend()
+        self.initialize_notes()
+
+    def edit_note(self, note_widget):
+        if note_widget.ids.edit_button.text == "SAVE":
+            note_widget.ids.body.readonly = True
+            note_widget.ids.delete_button.disabled = False
+            note_widget.ids.updatedate_button.disabled = False
+            note_widget.ids.body.background_color = [133/255, 144/255, 149/255, 0]
+            note_widget.ids.body.cursor_blink = False
+            note_widget.ids.body.text = note_widget.ids.body.text.strip()
+            self.database.edit_notes({'title': note_widget.ids.title.text, 'body': note_widget.ids.body.text})
+            note_widget.ids.edit_button.text = "EDIT"
+        elif note_widget.ids.edit_button.text == "EDIT":
+            note_widget.ids.body.readonly = False
+            note_widget.ids.delete_button.disabled = True
+            note_widget.ids.updatedate_button.disabled = True
+            note_widget.ids.body.background_color = [133/255, 144/255, 149/255, 1]
+            note_widget.ids.body.cursor_blink = True
+            note_widget.ids.edit_button.text = "SAVE"
+        else:
+            print("[  ERROR  ] An unexpected error has occured.")
+
+    def delete_note(self, note_widget):
+        self.database.delete_note({'title': note_widget.ids.title.text})
+        self.ids.scroll_box.remove_widget(note_widget)
+
+    def add_note(self):
+        get_title = NewNotePopup()
+        get_title.bind(on_dismiss=lambda a=get_title: self.add_note_popup_closed(a))
+        get_title.open()
+
+    def add_note_popup_closed(self, popup):
+        title_input = popup.ids.title_input.text.strip()
+        if title_input != "":
+            added = self.database.add_new_note({'title': popup.ids.title_input.text, 'body': ''})
+            if added:
+                self.initialize_notes()
+            else:
+                toast("Note with similar title already exists!", duration=1)
+
+    def update_date(self, note_widget):
+        self.database.update_date({'title': note_widget.ids.title.text})
+        self.initialize_notes()
+
+    def initialize_notes(self):
+        self.ids.scroll_box.clear_widgets()
+        notes = self.database.show_notes()
+        for note in notes:
+            note_widget = Note()
+            note_widget.ids.title.text = note.get('title')
+            note_widget.ids.body.text = note.get('body')
+            note_widget.ids.date.text = note.get('create_date')
+            self.ids.scroll_box.add_widget(note_widget)
 
 
-class Note():
+class Note(FloatLayout):
     pass
 
 
@@ -16,18 +76,13 @@ class NoteButton():
     pass
 
 
+class NewNotePopup(Popup):
+    pass
+
+
 class NotebookBackend():
     def __init__(self):
         self.OPERATOR = sql_operator()
-
-    def add_new_note(self, data):
-        title = data['title']
-        body = data['body']
-
-        creation_time = datetime.now().strftime("%c")
-
-        ## SQLITE QUERIES ## 
-
         create_note_table = """
         CREATE TABLE IF NOT EXISTS notebook (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -36,7 +91,14 @@ class NotebookBackend():
             create_date TEXT NOT NULL
         );
         """
+        self.OPERATOR.execute_query(create_note_table)
 
+    def add_new_note(self, data):
+        title = data['title']
+        body = data['body']
+
+        creation_time = datetime.now().strftime("%x")
+        
         check_existence = f"SELECT title FROM notebook;"
 
         add_note_in_table = f"""
@@ -46,20 +108,19 @@ class NotebookBackend():
             ('{title}', '{body}', '{creation_time}');
         """
 
-        self.OPERATOR.execute_query(create_note_table)
-
-        try:
-            title_column = self.OPERATOR.execute_read_query(check_existence)
-            title_column = [i[0] for i in title_column] # converting a list of tuple to a list of strings
-        except:
-            title_column = []
+        title_column = self.OPERATOR.execute_read_query(check_existence)
+        title_column = [i[0] for i in title_column] # converting a list of tuple to a list of strings
 
         if title in title_column:
-            print("Note with similar name already exists!")
+            return False
         else:
-            self.OPERATOR.execute_query(add_note_in_table)
+            try:
+                self.OPERATOR.execute_query(add_note_in_table)
+                return True
+            except:
+                return False
 
-    def delete_previous_note(self, data):
+    def delete_note(self, data):
         title = data["title"]
         delete_query = f"DELETE FROM notebook WHERE title = '{title}'"
         self.OPERATOR.execute_query(delete_query)
@@ -80,16 +141,30 @@ class NotebookBackend():
 
         return output_list[::-1]
 
-
     def edit_notes(self, data):
-        title = data["title"]
-        body = data["body"]
+        title = data.get("title")
+        body = data.get("body")
 
         edit_query = f"""
         UPDATE
             notebook
         SET
-            body = '{body}',
+            body = '{body}'
+        WHERE
+            title = '{title}'
+        """
+
+        self.OPERATOR.execute_query(edit_query)
+
+    def update_date(self, data):
+        title = data.get("title")
+        creation_time = datetime.now().strftime("%x")
+
+        edit_query = f"""
+        UPDATE
+            notebook
+        SET
+            create_date = '{creation_time}'
         WHERE
             title = '{title}'
         """
