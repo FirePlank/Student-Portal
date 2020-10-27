51b0c590ad1050eb6aa28119d0a3a5219b54cda0
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2020-10-27 20:53:48 +0530

    Settings appearance GUI added

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import kivy
 kivy.require('2.0.0')
 
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, SlideTransition, WipeTransition, FadeTransition, FallOutTransition, RiseInTransition, NoTransition, CardTransition
 from kivy.resources import resource_add_path
 from kivy.properties import ListProperty, StringProperty, NumericProperty
 from kivy.animation import Animation
@@ -45,20 +45,39 @@ class StudentPortal(MDApp):
 
     title = "Student Portal"
     color_theme = 'normal'
-    bg_color = ListProperty([71/255, 93/255, 102/255, 1])
-    tile_color = ListProperty([133/255, 144/255, 149/255, 1])
-    raised_button_color = ListProperty([144/255, 159/255, 165/255, 1])
-    text_color = ListProperty([0, 0, 0, 1])
+    bg_color = ListProperty([29/255, 29/255, 29/255, 1])
+    tile_color = ListProperty([40/255, 40/255, 40/255, 1])
+    raised_button_color = ListProperty([52/255, 52/255, 52/255, 1])
+    text_color = ListProperty([1, 1, 1, 1])
     title_text_color = ListProperty([1, 1, 1, 1])
     accent_color = ListProperty([0.5, 0.7, 0.5, 1])
     app_font = StringProperty(resource_path(os.path.join('data', 'fonts', 'Code2000', 'CODE2000.ttf')))
     cursor_width = NumericProperty(3)
 
     def build(self):
+        self.themes = {
+            'dark': self.color_theme_dark,
+            'light': self.color_theme_light,
+            'rgb': self.color_theme_rgb,
+            'custom': None
+        }
+
+        self.transitions = {
+            'slide': SlideTransition,
+            'wipe': WipeTransition,
+            'fade': FadeTransition,
+            'fall out': FallOutTransition,
+            'rise in': RiseInTransition,
+            'card': CardTransition,
+            'none': NoTransition
+        }
+
         if getattr(sys, 'frozen', False):
             from app.screens import mainmenu, wikipedia, notebook, translation, youtube, todo, books, settings, about
         else:
             from screens import mainmenu, wikipedia, notebook, translation, youtube, todo, books, settings, about
+
+        self.user_settings = settings.SettingsBackend().show_settings()
 
         self.root = ScreenManager()
         self.mainmenu = mainmenu.MainMenu()
@@ -81,47 +100,51 @@ class StudentPortal(MDApp):
             'settings': self.settings,
             'about': self.about
         }
+        self.themes['custom'] = self.settings.color_theme_custom
         self.root.switch_to(self.mainmenu)
-        self.unlock_dark_mode()
+        try:
+            self.root.transition = self.transitions.get(self.user_settings.get('page_transition'))()
+        except:
+            self.root.transition = SlideTransition()
+        self.themes.get(self.user_settings.get('theme'))()
+        Window.clearcolor = tuple(self.bg_color)
         return self.root
 
     def switch_screen(self, screen_name, direction='left'):
         self.root.transition.direction = direction
         self.root.switch_to(self.screens.get(screen_name))
 
-    def unlock_dark_mode(self):
+    def color_theme_dark(self):
         self.color_theme = 'dark'
         try:
             Animation.cancel_all(self)
         except:
             pass
         self.bg_color = [29/255, 29/255, 29/255, 1]
+        Window.clearcolor = tuple(self.bg_color)
         self.tile_color = [40/255, 40/255, 40/255, 1]
         self.raised_button_color = [52/255, 52/255, 52/255, 1]
         self.text_color = [1, 1, 1, 1]
         self.title_text_color = [1, 1, 1, 1]
         self.accent_color = [0.5, 0.7, 0.5, 1]
 
-    def color_theme_normal(self):
-        self.color_theme = 'normal'
+    def color_theme_light(self):
+        self.color_theme = 'light'
         try:
             Animation.cancel_all(self)
         except:
             pass
         self.bg_color = [71/255, 93/255, 102/255, 1]
+        Window.clearcolor = tuple(self.bg_color)
         self.tile_color = [133/255, 144/255, 149/255, 1]
         self.raised_button_color = [144/255, 159/255, 165/255, 1]
+        self.text_color = [0, 0, 0, 1]
         self.title_text_color = [1, 1, 1, 1]
         self.accent_color = [0.5, 0.7, 0.5, 1]
 
-    def color_theme_party(self):
-        self.color_theme = 'party'
-        self.bg_color = list(utils.get_random_color())
-        self.tile_color = list(utils.get_random_color())
-        self.raised_button_color = list(utils.get_random_color())
-        self.text_color = list(utils.get_random_color())
-        self.title_text_color = list(utils.get_random_color())
-        self.accent_color = list(utils.get_random_color())
+    def color_theme_rgb(self):
+        self.color_theme = 'rgb'
+        Window.clearcolor = (71/255, 93/255, 102/255, 1)
         def update_bg_anim(self):
             self.bg_color_animation = Animation(bg_color=utils.get_random_color(), duration=4.)
             self.bg_color_animation.bind(on_complete=lambda idk, a=self: update_bg_anim(a))
@@ -152,6 +175,30 @@ class StudentPortal(MDApp):
             self.accent_color_animation.bind(on_complete=lambda idk, a=self: update_accent_anim(a))
             self.accent_color_animation.start(self)
         update_accent_anim(self)
+
+    def color_theme_custom(self, user_settings):
+        self.color_theme = 'custom'
+        try:
+            Animation.cancel_all(self)
+        except:
+            pass
+        self.bg_color = self.parse_string_list(user_settings.get('bg_color'))
+        Window.clearcolor = tuple(self.bg_color)
+        self.tile_color = self.parse_string_list(user_settings.get('tile_color'))
+        self.raised_button_color = self.parse_string_list(user_settings.get('raised_button_color'))
+        self.text_color = self.parse_string_list(user_settings.get('text_color'))
+        self.title_text_color = self.parse_string_list(user_settings.get('title_text_color'))
+        self.accent_color = self.parse_string_list(user_settings.get('accent_color'))
+
+    def parse_string_list(self, to_parse):
+        parsed = []
+        for i in to_parse:
+            try:
+                parsed.append(float(i))
+            except ValueError:
+                num, denom = i.split('/')
+                parsed.append(float(num)/float(denom))
+        return parsed
 
 
 if __name__ == '__main__':
